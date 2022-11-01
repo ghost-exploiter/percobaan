fc17cb572e05fb5da2084c757b9b5fad8c7d3463
Arjunanda <dawatidawati31@gmail.com>
2022-11-01 12:14:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { QrReader } from 'react-qr-reader';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  const videoRef = useRef(null);
+
+  
+
+  const [data, setData] = useState("")
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: '100%', height: '100vh', position: 'fixed' } })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  };
+
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video autoPlay={true} ref={videoRef}></video>
+      <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+            
+          }}
+          scanDelay={10000000}
+          onScan={(v) => {
+            console.log(v)
+          }}
+          style={{ width: "600px", height: '200px'}}
+          containerStyle={{ maxWidth: '250px', paddingTop: "0px", paddingBottom: "0px", width: '500px',  borderRadius: "20px", position: 'absolute', top: '0'   }}
+          videoStyle={{  margin: '12px auto', objectFit: "cover", borderRadius: "20px" }}
+        />
+        <p>{data}</p>
     </div>
   );
 }
